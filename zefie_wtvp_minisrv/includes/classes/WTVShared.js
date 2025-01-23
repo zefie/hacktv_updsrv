053066f73a1fca0c4a69fb339f209b25bdf14766
zefie <zefie@zefie.net>
2025-01-23 14:09:36 -0500

    start work on #16

--- zefie_wtvp_minisrv/includes/classes/WTVShared.js
+++ zefie_wtvp_minisrv/includes/classes/WTVShared.js
@@ -73,7 +73,7 @@ class WTVShared {
 
     // check if the SSID has a valid checksum
     checkSSID(ssid) {
-        if (ssid.slice(-2) == this.getSSIDCRC(ssid))
+        if ((ssid.slice(-2) == this.getSSIDCRC(ssid)) && ssid.length == 16)
             return true;
         return false;
     }
